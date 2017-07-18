d6eb4ba5a8a7d56588e4629bc42a00609de989ae
Nizar Lakhdhir <nizarl@chenmed.com>
2017-07-18 12:19:09 -0400

    using errormessage component to handle table error messages

--- src/medications/medications.component.js
+++ src/medications/medications.component.js
@@ -33,11 +33,6 @@ function medicationsCtrl(EventService, RestClientService, PathService, PatientSe
 
     medModel.$onInit = function () {
         fetchPatientMeds(RestClientService, pathInfo, patientService).then(function (resp) {
-            //temp: remove after test error message with no data
-            var resp = resp;
-            resp.data = [];
-
-
             if (resp.data.length > 0) {
                 var medVM = buildActiveMeds(resp.data);
                 medModel.patientMeds = medVM;
